fa1860c3cb0750d7d45a90abaadb7e3acaa60017
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-08-04 15:47:23 -0500

    Add get by id, put and delete request

--- src/index.js
+++ src/index.js
@@ -21,6 +21,15 @@ app.get('/api/users', async (_, res) => {
   return res.status(200).json(users)
 })
 
+app.get('/api/users/:id', async (_, res) => {
+  const { id } = req.params
+  const userFound = await prisma.user.findUnique({
+    where: { id }
+  })
+
+  return res.status(200).json(userFound)
+})
+
 app.post('/api/users', async (req, res) => {
   const data = req.body
   const userCreated = await prisma.user.create({
@@ -34,6 +43,32 @@ app.post('/api/users', async (req, res) => {
   })
 
   return res.status(201).json(userCreated)
+})
+
+app.put('/api/users/:id', async (req, res) => {
+  const data = req.body
+  const { id } = req.params
+  const userUpdated = await prisma.user.update({
+    where: { id: id },
+    data: {
+      name: data.name,
+      address: data.address,
+      email: data.email,
+      phone: data.phone,
+      role: data.role,
+    }
+  })
+
+  return res.status(200).json(userUpdated)
+})
+
+app.delete('/api/users/:id', async (req, res) => {
+  const { id } = req.params
+  const userDeleted = await prisma.user.delete({
+    where: { id: id }
+  })
+
+  return res.status(200).json(userDeleted)
 })
 
 app.listen(port, () => {
